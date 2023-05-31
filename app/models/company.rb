1f23d95f313ce267ae6bb73c24d9db47a27526f1
Victor <victor.ireri@student.moringaschool.com>
2023-05-31 11:58:24 +0300

    labs

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,10 @@
 class Company < ActiveRecord::Base
-
+    has_many :freebies
+    has_many :devs , through: :freebies
+    def give_freebie(dev, item_name, value)
+     freebies.create(dev: dev, item_name: item_name, value: value)
+    end
+    def self.oldest_company
+     self.order(:founding_year).first
+    end
 end
--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,14 @@
 class Dev < ActiveRecord::Base
-
+    has_many :freebies
+  has_many :companies, through: :freebies
+ def received_one(item_name)
+  freebies.exists?(item_name: item_name)
+ end
+ def give_away(dev, freebie)
+    if freebie.dev == self
+      freebie.update(dev: dev)
+    else
+      puts "Cannot give away the freebie. It does not belong to the current dev."
+end
+end
 end
